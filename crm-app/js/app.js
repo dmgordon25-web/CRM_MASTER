5915faeb2ec0790e1d82601ab249dfa90018613d
dmgordon25-web <dmgordon25@gmail.com>
2025-11-09 02:32:32 -0500

    Import theme injector during app boot

--- crm-app/js/app.js
+++ crm-app/js/app.js
@@ -1,6 +1,7 @@
 import './dashboard/index.js';
 import './dashboard/kpis.js';
 import './relationships/index.js';
+import './ui/styles/inject_theme.js';
 import { openPartnerEditModal, closePartnerEditModal } from './ui/modals/partner_edit/index.js';
 import { ensureActionBarPostPaintRefresh } from './ui/action_bar.js';
 import { normalizeStatus } from './pipeline/constants.js';
